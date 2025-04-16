31cfe9f0bb5d0b32da307d0b870419c68bfe797d
Nesredin Ahmed <ahmednesredin@icloud.com>
2025-04-15 22:54:55 -0700

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,55 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.awt.desktop.SystemEventListener;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String welcome = "Big Tex says \n\t\"Howdy\"!";
+        String formatted = "Name:\tGlen\nTitle  :\tDeveloper\nJob:\tUnemployoed";
+        System.out.println(welcome);
+        System.out.println(formatted);
+        String s1 = "abc";
+        String s2 = "ABC";
+        String s3 = "A" + "B" + "C";
+        if (s1.equalsIgnoreCase(s2)) {
+            System.out.println("s1 equals s2 is TRUE");  // ✅ This will print
         }
+
+        if (s1.equalsIgnoreCase(s3)) {
+            System.out.println("s1 equals s3 is TRUE");  // ✅ This will also print
+        }
+        String fname = "Nesredin Adem";
+        int lenght = fname.length();
+        System.out.println("Length Of:\t" + fname + " is =\t"+ lenght);
+        String username = "  danaw  ";
+        username = username.trim();
+        username = username.toUpperCase(Locale.ROOT);
+        System.out.println("User Name:[" + username + "]");
+
+        String discountCode = "SAVE-50";
+        if (discountCode.endsWith("-50")) {// this is true
+            String print = ("Eligiable for Discount");
+            print = print.toUpperCase();
+            System.out.println(print);
+
+            // tracking
+//            Scanner scan = new Scanner(System.in);
+//            System.out.printf("Enter  A Word:");
+//            String trackingCode = scan.nextLine().trim();
+//            System.out.printf("Enter The Number of Character:");
+//            int charnumb = scan.nextInt();
+//            char hasShipped = trackingCode.charAt(charnumb);
+//            System.out.println("Tracking Character At:" + charnumb + " is " + hasShipped );
+        }
+        String input = "Alice,Bob,Charlie,David,Nasri,Adem,,,,n";
+        String[] names = input.split(",");
+        for (String name : names) {
+            System.out.println(name);
+        }
+
+
+
     }
+
 }
